e5cf946c1ffaae6aae5cf2059f5033a0c01b7fc6
MateusChitolina <mateus.ex.chitolina@gmail.com>
2024-06-13 19:43:57 -0300

    MsgCase setup

---
+++ MsgCase/src/msg/EstimatedFundsReport.java
@@ -0,0 +1,17 @@
+package msg;
+
+import java.io.*;
+
+public class EstimatedFundsReport
+{
+
+  public static void printReport ()
+  //
+  // computes total funds available for purchasing new mortgages in the current week.
+  //
+  {
+    System.out.println ("Funds available: " + MSGApplication.getEstimatedFundsForWeek());
+    UserInterface.pressEnter();
+  }  // printReport
+
+}
